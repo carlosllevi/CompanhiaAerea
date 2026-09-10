0686007a7a40943545c2a45e191ecbe4fabee764
Rafael Mendes <rafaeloms07@gmail.com>
2026-09-10 18:57:24 -0300

    Finaliza a classe Voo e cria comentários para futuros ajustes

--- CompanhiaAerea/Passagem.java
+++ CompanhiaAerea/Passagem.java
@@ -4,7 +4,11 @@ class Passagem {
   private StatusPassagem status;
   private Passageiro passageiro;
   private Voo voo;
-  protected Assento assento;
+  protected Assento assento; // no diagrama está private, mas nessa condição o programa não compila.
+  /*
+  SOLUCOES:
+  1 - Manter protected e alterar o digrama.
+  2 - Cria métodos getters.*/
 
    public double calcularValorTotal() {
     return 0;
--- CompanhiaAerea/Voo.java
+++ CompanhiaAerea/Voo.java
@@ -1,3 +1,15 @@
+import java.time.LocalDateTime;
+
 public class Voo {
-    
+    private String numero;
+    private String origem;
+    private String destino;
+    private LocalDateTime dataHora;
+    private int qtdTripulantes;
+    private int qtdTripulacao; // alterar para qtdPassageiros
+
+    public boolean verificarDisponibilidade() {
+        return true;
+    }
+
 }
